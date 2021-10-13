9ff9352ca5ac5496e1585625b8b96bf08ab71381
DvirDatner <dat.dvi@gmail.com>
2021-10-13 09:37:41 +0300

    finished certificates component

--- src/app/certificates/certificates.component.ts
+++ src/app/certificates/certificates.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperOptions } from 'swiper';
 
 @Component({
   selector: 'app-certificates',
   templateUrl: './certificates.component.html',
-  styleUrls: ['./certificates.component.css']
+  styleUrls: ['./certificates.component.css'],
 })
 export class CertificatesComponent implements OnInit {
+  config: SwiperOptions = {
+    effect: 'flip',
+    grabCursor: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
